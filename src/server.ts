68e10cb914d858755742f2329ad7519a055503f0
Yuri Rennan <yurirennan14@gmail.com>
2021-01-30 17:46:49 -0300

    Adicionado eslint e editor.config

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,6 @@ import express from 'express';
 const server = express();
 const port = 3333;
 
-server.get('/', (req, res) => {
-  return res.json({message: 'Hello World'});
-});
-
+server.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 server.listen(port);
